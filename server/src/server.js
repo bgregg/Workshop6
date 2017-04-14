48805a7e2b982718ad05e9c1cc8feafd545d4694
Brian Gregg <bgregg@umass.edu>
2017-04-14 11:11:08 -0400

    static

--- server/src/server.js
+++ server/src/server.js
@@ -1,32 +1,13 @@
 // Imports the express Node module.
 var express = require('express');
-var util = require('./util');
-var rev = util.reverseString;
 var bodyParser = require('body-parser');
 // Creates an Express server.
 var app = express();
 
 app.use(bodyParser.text());
-
-// Defines what happens when it receives the `GET /` request
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+app.use(express.static('../client/build'));
 
 // Starts the server on port 3000!
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
-});
-
-// Handle POST /reverse [data]
-app.post('/reverse', function (req, res) {
-  // How do we get the input text?
-  // How do we send the output text?
-  if (typeof(req.body) === 'string') {
-    var reversed = rev(req.body);
-    res.send(reversed);
-  } else {
-    // POST did not contain a string. Send an error code back!
-    res.status(400).end()
-  }
 });
